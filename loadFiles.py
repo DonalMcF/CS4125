8d7407d3b97f48dac64c25fccd6bdd5f15577e05
DonalMcF <81565892+DonalMcF@users.noreply.github.com>
2021-11-25 15:48:19 +0000

    Initial file upload

---
+++ loadFiles.py
@@ -0,0 +1,77 @@
+accounts = []
+gameLibrary = []
+
+class accountsFormat:
+    def __init__(self, name, password, accountType):
+        self.name = name
+        self.password = password
+        self.accountType = accountType
+        
+
+class gameLibraryFormat:
+    def __init__(self, name, genre, newRelease, pPrice):
+        self.name = name
+        self.genre = genre
+        self.newRelease = newRelease
+        self.pPrice = pPrice
+
+with open('accounts.txt','r') as f:
+    textlines = f.readlines()
+    
+    for i in textlines:
+        line = i.split(", ")
+        accounts.append(accountsFormat(line[0],line[1],line[2].replace("\n","")))
+    f.close()    
+
+with open('gameLibrary.txt','r') as f:
+    textlines = f.readlines()
+    
+    for i in textlines:
+        line = i.split(", ")
+        gameLibrary.append(gameLibraryFormat(line[0],line[1],line[2],line[3].replace("\n","")))
+    f.close() 
+    
+def updateMembership(currentUser, userType):
+    if userType == "user":
+        userType = "member"
+        outF = open('accounts.txt', 'w')
+        linenumber = len(accounts)
+        i = 0
+        for obj in accounts:
+            i = i + 1
+            if currentUser == obj.name and i == len(accounts):
+                newline = obj.name + ", " + obj.password + ", member"
+                outF.write(newline)
+            elif currentUser != obj.name and i == len(accounts):
+                newline = obj.name + ", " + obj.password + ", " + obj.accountType
+                outF.write(newline)
+            elif currentUser == obj.name:
+                newline = obj.name + ", " + obj.password + ", member\n"
+                outF.write(newline)
+            else:
+                newline = obj.name + ", " + obj.password + ", " + obj.accountType + "\n"
+                outF.write(newline)
+        outF.close()
+        userType = "member"
+        
+    elif userType == "member":
+        outF = open('accounts.txt', 'w')
+        linenumber = len(accounts)
+        i = 0
+        for obj in accounts:
+            i = i + 1
+            if currentUser == obj.name and i == len(accounts):
+                newline = obj.name + ", " + obj.password + ", user"
+                outF.write(newline)
+            elif currentUser != obj.name and i == len(accounts):
+                newline = obj.name + ", " + obj.password + ", " + obj.accountType
+                outF.write(newline)
+            elif currentUser == obj.name:
+                newline = obj.name + ", " + obj.password + ", user\n"
+                outF.write(newline)
+            else:
+                newline = obj.name + ", " + obj.password + ", " + obj.accountType + "\n"
+                outF.write(newline)
+        outF.close()
+        userType = "user"
+    return userType
