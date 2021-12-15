5640b83bfdeb44c9a46a52c3b6c6a7ca847ed0d5
Jamie <79202708+Jamie-Doupe@users.noreply.github.com>
2021-12-15 13:06:22 +0000

    Update calcDiscount.py

--- calcDiscount.py
+++ calcDiscount.py
@@ -1,26 +1,26 @@
-import discountFactory
-def calcDiscount():
-    tempName = discountFactory.discountFactory.name
-    tempUserType = discountFactory.discountFactory.userType
-    tempGenre = discountFactory.discountFactory.genre
-    tempNewRelease = discountFactory.discountFactory.newRelease
-    tempPrice = float(discountFactory.discountFactory.price)
-    tempDiscount = 0.0
-    if(tempUserType == "member" or tempUserType == "admin") and not tempNewRelease == "yes" and tempGenre == "teens":
+import discount_factory
+def calc_discount():
+    temp_name = discount_factory.discount_factory.name
+    temp_user_type = discount_factory.discount_factory.user_type
+    user_genre = discount_factory.discount_factory.genre
+    temp_new_release = discount_factory.discount_factory.new_release
+    temp_price = float(discount_factory.discount_factory.price)
+    temp_discount = 0.0
+    if(temp_user_type == "member" or temp_user_type == "admin") and not temp_new_release == "yes" and user_genre == "teens":
         print("Applying member discount of 25% for teens game")
-        tempDiscount = tempPrice * 0.75
-    elif(tempUserType == "member" or tempUserType == "admin") and not tempNewRelease == "yes" and tempGenre == "childrens":
+        temp_discount = temp_price * 0.75
+    elif(temp_user_type == "member" or temp_user_type == "admin") and not temp_new_release == "yes" and user_genre == "childrens":
         print("Applying member discount of 35% for childrens game")
-        tempDiscount = tempPrice * 0.65
-    elif(tempUserType == "member" or tempUserType == "admin") and tempNewRelease == "yes" and tempGenre == "teens":
+        temp_discount = temp_price * 0.65
+    elif(temp_user_type == "member" or temp_user_type == "admin") and temp_new_release == "yes" and user_genre == "teens":
         print("Applying member discount of 15% for new teens game")
-        tempDiscount = tempPrice * 0.85
-    elif(tempUserType == "member" or tempUserType == "admin") and tempNewRelease == "yes" and tempGenre == "childrens":
+        temp_discount = temp_price * 0.85
+    elif(temp_user_type == "member" or temp_user_type == "admin") and temp_new_release == "yes" and user_genre == "childrens":
         print("Applying member discount of 25% for new childrens")
-        tempDiscount = tempPrice * 0.75
-    if tempDiscount == tempPrice:
-        print(tempName, ", €", tempPrice, sep = '')
+        temp_discount = temp_price * 0.75
+    if temp_discount == temp_price:
+        print(temp_name, ", €", temp_price, sep = '')
         
     else:
-        print(tempName, ", Original price €", tempPrice, ", Discounted Price €", tempDiscount, sep = '')
-    discountFactory.discountFactory.totalDiscount = tempDiscount
+        print(temp_name, ", Original price €", temp_price, ", Discounted Price €", temp_discount, sep = '')
+    discount_factory.discount_factory.total_discount = temp_discount
